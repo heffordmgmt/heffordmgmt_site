ad63436e44c652b2d8503416c69d0522cc2f5727
Ryan Hefford <ryan@heffordmgmt.com>
2026-07-09 17:57:57 -0400

    Initial website

---
+++ script.js
@@ -0,0 +1,11 @@
+const revealEls = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.14 });
+
+revealEls.forEach((el) => observer.observe(el));
